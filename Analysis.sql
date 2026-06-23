b4d67f8f37e9c8ece32763fcbd99b2745052927b
EJ De Vera <ejdevera220@gmail.com>
2026-06-23 17:18:39 +0100

    Analysis.sql

--- Analysis.sql
+++ Analysis.sql
@@ -8,18 +8,54 @@ FROM SampleSuperstore;
 ==========================
 ### REGIONAL ANALYSIS ###
 ==========================
+  
+---- Region Analysis ----
+SELECT region, sum(sales) AS Total_Sales, 
+sum(profit) AS Total_Profit, 
+round((sum(profit)/sum(sales))*100,2) AS Profit_Margin_Percentage, 
+count(order_id) AS Total_Orders, 
+round(avg(discount)*100,2) AS Average_Discount_Percentage FROM SampleSuperstore
+GROUP BY region
+ORDER BY Profit_Margin_Percentage DESC;
+GROUP BY state
 
 ---- State Analysis ----
 SELECT state,
 sum(sales) AS Total_Sales,
-sum(profit) AS Total_Profit FROM SampleSuperstore
+sum(profit) AS Total_Profit
+FROM SampleSuperstore
 GROUP BY state
+ORDER BY Total_Sales DESC;
+
+---- City Analysis ----
+SELECT city, 
+sum(sales) AS Total_Sales,
+sum(profit) AS Total_Profit
+FROM SampleSuperstore
+GROUP BY city 
 ORDER BY Total_Sales DESC;
 
 ---- Profit Margin Analysis by State ----
 SELECT state,
 sum(sales) AS Total_Sales,
-sum(profit) AS Total_Profits,
+sum(profit) AS Total_Profit,
 round((sum(profit)/sum(sales))*100,2) AS Profit_Margin_Percentage FROM SampleSuperstore
-GROUP BY state
 ORDER BY Profit_Margin_Percentage DESC;
+
+==========================
+### PRODUCT ANALYSIS ###
+==========================
+
+---- Category Analysis ----
+SELECT category,sum(sales) AS Total_Sales,
+sum(profit) AS Total_Profits,
+COUNT(order_id) AS Total_Orders,
+round((sum(profit)/sum(sales))*100,2) AS Profit_Margin_Percentage,
+round(avg(discount)*100,2) AS Average_Discount_Percentage, 
+max(sales) AS Highest_Product_Sale,
+min(profit) AS Lowest_Product_Profit
+FROM SampleSuperstore
+GROUP BY category
+ORDER BY Total_Sales DESC;
+  
+
