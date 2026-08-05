ac1bb9f5fc53573a667e9e404ef4c094ba589344
EJ De Vera <ejdevera220@gmail.com>
2026-08-05 23:45:10 +0100

    Analysis.sql

--- Analysis.sql
+++ Analysis.sql
@@ -121,10 +121,165 @@ FROM SampleSuperstore
 GROUP BY customer_name
 ORDER BY Profit_Margin DESC;
 
----- Customers with Best Profit Margins  ----
+---- Top Customer Loss Making Orders ----
+SELECT customer_name,
+ROUND(sum(sales),2) AS Total_Sales,
+ROUND(sum(profit),2) AS Total_Profit,
+sum(quantity) AS Units_Sold
+FROM SampleSuperstore
+GROUP BY customer_name
+HAVING Total_Profit < 0
+ORDER BY Total_Profit ASC;
 
+---- Customers with the most orders ----
+SELECT customer_name,
+COUNT(DISTINCT order_id) AS Total_Orders,
+ROUND(sum(sales),2) AS Total_Sales,
+ROUND(sum(profit),2) AS Total_Profit,
+sum(quantity) AS Units_Sold,
+ROUND((sum(profit)/sum(sales))*100,2) AS Profit_Margin_Percentage
+FROM SampleSuperstore
+GROUP BY customer_name
+ORDER BY Total_Orders DESC;
 
+==========================
+### SALES ANALYSIS ###
+==========================
 
+---- Segment Performance ----
+SELECT segment,
+ROUND(sum(sales),2) AS Total_Sales,
+ROUND(sum(profit),2) AS Total_Profit,
+COUNT(DISTINCT order_id) as Total_Orders,
+sum(quantity) AS Total_Units_Sold,
+ROUND((sum(profit)/sum(sales))*100,2) AS Profit_Margin_Percentage,
+ROUND((AVG(discount))*100,2) AS Average_Discount_Percentage
+FROM SampleSuperstore
+GROUP BY segment
+ORDER BY Total_Sales DESC;
 
+---- Ship Mode Performance ----
+SELECT ship_mode,
+ROUND(sum(sales),2) AS Total_Sales,
+ROUND(sum(profit),2) AS Total_Profit,
+COUNT(DISTINCT order_id) as Total_Orders,
+sum(quantity) AS Total_Units_Sold,
+ROUND((sum(profit)/sum(sales))*100,2) AS Profit_Margin_Percentage,
+ROUND((AVG(discount))*100,2) AS Average_Discount_Percentage
+FROM SampleSuperstore
+GROUP BY ship_mode
+ORDER BY Total_Sales DESC;
 
+==========================
+### DISCOUNT ANALYSIS ###
+==========================
 
+---- Profit by Discount Level ----
+SELECT discount,
+ROUND((sum(profit)/sum(sales))*100,2) AS Profit_Margin_Percentage,
+ROUND(sum(sales),2) AS Total_Sales,
+ROUND(sum(profit),2) AS Total_Profit,
+COUNT(DISTINCT order_id) AS Total_Orders,
+sum(quantity) AS Total_Units_Sold
+FROM SampleSuperstore
+GROUP BY discount
+ORDER BY discount ASC;
+
+---- Discount by Category ----
+SELECT category,
+ROUND(AVG(discount)*100,2) AS Average_Discount_Percentage,
+ROUND(sum(profit)/sum(sales)*100,2) AS Profit_Margin_Percentage,
+ROUND(sum(sales),2) AS Total_Sales,
+ROUND(sum(profit),2) AS Total_Profit,
+COUNT(DISTINCT order_id) AS Total_Orders,
+sum(quantity) AS Total_Units_Sold
+FROM SampleSuperstore
+GROUP BY category
+ORDER BY Profit_Margin_Percentage DESC;
+
+---- Discount by Sub-Category ----
+SELECT sub_category,
+ROUND(AVG(discount)*100,2) AS Average_Discount_Percentage,
+ROUND(sum(profit)/sum(sales)*100,2) AS Profit_Margin_Percentage,
+ROUND(sum(sales),2) AS Total_Sales,
+ROUND(sum(profit),2) AS Total_Profit,
+COUNT(DISTINCT order_id) AS Total_Orders,
+sum(quantity) AS Total_Units_Sold
+FROM SampleSuperstore
+GROUP BY sub_category
+ORDER BY Profit_Margin_Percentage DESC;
+
+---- Discount by Region ----
+SELECT region,
+ROUND(AVG(discount)*100,2) AS Average_Discount_Percentage,
+ROUND(sum(profit)/sum(sales)*100,2) AS Profit_Margin_Percentage,
+ROUND(sum(sales),2) AS Total_Sales,
+ROUND(sum(profit),2) AS Total_Profit,
+COUNT(DISTINCT order_id) AS Total_Orders,
+sum(quantity) AS Total_Units_Sold
+FROM SampleSuperstore
+GROUP BY region
+ORDER BY Profit_Margin_Percentage DESC;
+
+==========================
+### ORDER ANALYSIS ###
+==========================
+
+---- TOP 10 Orders by Sales ----
+SELECT order_id,
+customer_name,
+ROUND(sum(sales),2) AS Total_Sales,
+ROUND(sum(profit),2) AS Total_Profit,
+sum(quantity) AS Total_Units_Sold
+FROM SampleSuperstore
+GROUP BY order_id
+ORDER BY Total_Sales DESC
+LIMIT 10;
+
+---- Most Profitable Orders ----
+SELECT order_id,
+ROUND(sum(sales),2) AS Total_Sales,
+ROUND(sum(profit),2) AS Total_Profit,
+ROUND(sum(profit)/NULLIF(sum(sales),0)*100,2) AS Profit_Margin_Percentage
+FROM SampleSuperstore
+GROUP BY order_id
+HAVING Profit_Margin_Percentage = 50;
+
+---- Lost Making Orders ----
+SELECT order_id,
+ROUND(sum(sales),2) AS Total_Sales,
+ROUND(sum(profit),2) AS Total_Profit,
+ROUND(sum(profit)/NULLIF(sum(sales),0)*100,2) AS Profit_Margin_Percentage,
+sum(quantity) AS Total_Units_Sold
+FROM SampleSuperstore
+GROUP BY order_id
+HAVING Total_Profit < 0
+ORDER BY Total_profit ASC;
+
+---- TOP 10 Orders by Number of Products ----
+SELECT order_id,
+customer_name,
+COUNT(DISTINCT product_name) AS Different_Products,
+ROUND(sum(sales),2) AS Total_Sales,
+ROUND(sum(profit),2) AS Total_Profit,
+sum(quantity) AS Total_Units_Sold,
+ROUND((sum(profit)/sum(sales))*100,2) AS Profit_Margin_Percentage
+FROM SampleSuperstore
+GROUP BY order_id
+ORDER BY Different_Products DESC
+LIMIT 10;
+
+==========================
+### TIME ANALYSIS ###
+==========================
+  
+SELECT order_date,
+order_id,
+ROUND(sum(sales),2) AS Total_Sales,
+ROUND(sum(profit),2) AS Total_Profit,
+ROUND(sum(profit)/sum(sales)*100,2) AS Profit_Margin_Percentage
+from SampleSuperstore
+GROUP BY order_date
+HAVING order_date LIKE '%2016%'
+ORDER BY Total_Sales DESC
+LIMIT 10;
